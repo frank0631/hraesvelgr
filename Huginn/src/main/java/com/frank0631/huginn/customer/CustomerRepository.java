aa85115ab2da2aebf159895131ccae2cbcd075a9
frank0631 <frank0631@gmail.com>
2015-07-14 12:04:23 -0400

    HATEOAS customer repo

--- Huginn/src/main/java/com/frank0631/huginn/customer/CustomerRepository.java
+++ Huginn/src/main/java/com/frank0631/huginn/customer/CustomerRepository.java
@@ -1,13 +1,17 @@
 package com.frank0631.huginn.customer;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
 /**
  * Created by frank0631 on 5/12/15.
  */
+@RepositoryRestResource(collectionResourceRel = "customers", path = "customers")
 public interface CustomerRepository extends CrudRepository<CustomerEntity, Long> {
 
-    List<CustomerEntity> findByLastName(String lastName);
+    List<CustomerEntity> findByLastName(@Param("lastName") String lastName);
 }
