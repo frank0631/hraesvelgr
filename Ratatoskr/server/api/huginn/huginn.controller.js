3f95a6687282afe61d82d9b434174d2975e03019
frank0631 <frank0631@gmail.com>
2015-08-15 23:14:57 -0400

    move Port and Address decleration to controller from local.env

--- Ratatoskr/server/api/huginn/huginn.controller.js
+++ Ratatoskr/server/api/huginn/huginn.controller.js
@@ -4,8 +4,8 @@ var _ = require('lodash');
 
 // Get list of huginns
 exports.index = function(req, res) {
-  var port = process.env.HUGG_PORT;
-  var address = process.env.HUGG_ADDR;
+  var port = process.env.HUGG_PORT_9000_TCP_PORT || '9001';
+  var address = process.env.HUGG_PORT_9000_TCP_ADDR || 'localhost';
   var url = address+":"+port;
   //console.log(JSON.stringify({"port": port, "address":address}));
   res.json({"port": port, "address": address, "url": url});
--- Ratatoskr/server/config/local.env.sample.js
+++ Ratatoskr/server/config/local.env.sample.js
@@ -5,14 +5,7 @@
 //
 // You will need to set these on the server you deploy to.
 
-var HUGG_PORT = process.env.HUGG_PORT_9000_TCP_PORT || 'localhost';
-var HUGG_ADDR = process.env.HUGG_PORT_9000_TCP_ADDR || '9000';
-
 module.exports = {
-
-  //Docker Loaded environment variables for Huginn
-  HUGG_PORT: HUGG_PORT,
-  HUGG_ADDR: HUGG_ADDR,
 
   DOMAIN:           'http://localhost:9000',
   SESSION_SECRET:   'ratatoskr-secret',
@@ -20,4 +13,3 @@ module.exports = {
   // Control debug level for modules using visionmedia/debug
   DEBUG: ''
 };
-
